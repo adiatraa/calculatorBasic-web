2b9d20d61d44804e1eb1693870dfb20f91b72e08
adiatraa <adiatrahman31@gmail.com>
2024-05-22 16:59:51 +0700

    Updating the logic for add number to display and selectOperator logic (with remove the number when select operator)

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 let displayNum = document.getElementById("display-num");
-displayNum.textContent = "";
+displayNum.textContent = ""; //Make sure display-num clear
 
-let currentNumber = "";
-let firstNumber = null;
-let secondNumber = null;
+let firstNumber = ""; //First Number null
+let currentOperator = ""; //Current Operator null
+let isSecondNumber = false; //Boolean variable for making previous number dissapear before adding secondNumber
 
-let operator = null;
 
 function addNumber(number){
-    if (displayNum.textContent === ""){
-        displayNum.textContent = number;
-    } else {
-        displayNum.textContent += number;
+    // If-else funct for the user after input the secondNumber 
+    // then the display will be clear and isSecondNumber will be false because it will be 
+    // change to firstNumber again
+    if(isSecondNumber){
+        displayNum.textContent = "";
+        isSecondNumber = false;
     }
-    currentNumber = displayNum.textContent;
+    displayNum.textContent += number;
 } 
 
 function selectOperator(operator){
-    firstNumber  = parseFloat(currentNumber);
-    operator = op;
-    currentNumber = "";
+    firstNumber  = displayNum.textContent;
+    currentOperator = operator;
+    displayNum.textContent = "";
+    isSecondNumber = true; //Activate the secondNumber after select the operator
 }
-
