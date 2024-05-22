7dcbed8926197dc4291454241f506bad7c4e69db
adiatraa <adiatrahman31@gmail.com>
2024-05-22 17:30:25 +0700

    Add function calculate for all operation using switch case and Add clearDisplay function for AC button

--- index.js
+++ index.js
@@ -5,21 +5,52 @@ let firstNumber = ""; //First Number null
 let currentOperator = ""; //Current Operator null
 let isSecondNumber = false; //Boolean variable for making previous number dissapear before adding secondNumber
 
-
-function addNumber(number){
-    // If-else funct for the user after input the secondNumber 
-    // then the display will be clear and isSecondNumber will be false because it will be 
-    // change to firstNumber again
-    if(isSecondNumber){
-        displayNum.textContent = "";
-        isSecondNumber = false;
-    }
-    displayNum.textContent += number;
-} 
-
-function selectOperator(operator){
-    firstNumber  = displayNum.textContent;
-    currentOperator = operator;
+function addNumber(number) {
+  // If-else funct for the user after input the secondNumber
+  // then the display will be clear and isSecondNumber will be false because it will be
+  // change to firstNumber again
+  if (isSecondNumber) {
     displayNum.textContent = "";
-    isSecondNumber = true; //Activate the secondNumber after select the operator
+    isSecondNumber = false;
+  }
+  displayNum.textContent += number;
+}
+
+function selectOperator(operator) {
+  firstNumber = displayNum.textContent;
+  currentOperator = operator;
+  displayNum.textContent = "";
+  isSecondNumber = true; //Activate the secondNumber after select the operator
+}
+
+function calculate() {
+  let secondNumber = displayNum.textContent;
+  let result;
+
+  switch (currentOperator) {
+    case "+":
+      result = parseFloat(firstNumber) + parseFloat(secondNumber);
+      break;
+    case "-":
+      result = parseFloat(firstNumber) - parseFloat(secondNumber);
+      break;
+    case "*":
+      result = parseFloat(firstNumber) * parseFloat(secondNumber);
+      break;
+    case "/":
+      if (secondNumber === "0") {
+        result = alert("Error Undefined : Division by Zero");
+      } else {
+        result = parseFloat(firstNumber) / parseFloat(secondNumber);
+      }
+      break;
+  }
+  displayNum.textContent = result;
+  firstNumber = "";
+  currentOperator = "";
+  isSecondNumber = false;
+}
+
+function clearDisplay() {
+  displayNum.textContent = "";
 }
